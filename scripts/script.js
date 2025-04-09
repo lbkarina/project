5141c31f3340671763e1e16fa3f369262430b922
lbkarina <karinalubyh6@gmail.com>
2025-04-09 14:43:20 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
 'use strict'
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно')
+    // *  1. Начало.
+    // *  2. Получаем все элементы изображения с описанием.
+    // *  3. Для каждого изображения (проверяем есть ли таоке изображение):
+    // *     3.1. Добавляем обработчик наведения курсора на изображение:
+    // *         3.1.1. Да:
+    // *             3.1.1.1. показываем текст при наведении.
+    // *         3.1.2. Нет: продолжаем.
+    // *     3.2. Добавляем обработчик курсор уходит с изображения:
+    // *         3.2.1. Да:
+    // *             3.2.1.1. Скрывем элементы с описанием.
+    // *         3.2.2. Нет: продолжаем.
+    // *  4. Конец  
+    
+    const bestseller = document.querySelectorAll(".best-seller__item") // querySelectorAll
+    
+    bestseller.forEach((item, index) => {
+        const bestsellerText = document.querySelectorAll('.best-seller__hover')
+        
+        item.addEventListener('mouseenter', () => {
+            item.style.opacity = 0.5;
+            bestsellerText[index].removeAttribute('hidden');
+    });
+    
+    item.addEventListener('mouseleave', () => {
+        item.style.opacity = 1
+        bestsellerText[index].setAttribute('hidden', true);
+    });
+});
 });
